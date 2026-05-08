40a38edcbc2e84e903739ae96326492fedf727d5
poyenChen <125422316+poyeChen@users.noreply.github.com>
2026-05-08 14:27:20 +0900

    Update run_MD.sh

--- MACELES_model/Evaluation_files/run_MD.sh
+++ MACELES_model/Evaluation_files/run_MD.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-python -u /home/poyen/ef-md-workflow-main/3_MLP_MD_with_E/MLP_MD_E_only_MACE_new.py \
-    --input "structure.xyz" \
-    --MLP_model "/home/poyen/BTO_MLFF/MACE/MACE_large_100.model" \
+python -u ./MLP_MD_E.py \
+    --input "POSCAR" \
+    --MLP_model "../BTO_MACELES.model" \
     --ini_temperature 250 \
-    --total_step 600000 \
+    --total_step 400000 \
     --device "cuda" \
     --ensemble_type "NPT" \
-    --BEC_model "/home/poyen/BTO_MLFF/MACE/MACEField_from_BTO/MACE_Field.model" \
+    --BEC_model "../BTO_Equivar_model.model" \
     --efield_mode "triangle" \
     --bec_update_interval 10 \
     --efield_direction "x" \
-    --ref ./POSCAR_ref \
-    --Emax 0.0015 \
-#    --positive_first \
-#    --Elist -0.15000 \
+    --Emax 0.00100 \
+    --use_Equivar
